e7a2de6607a825b30b2a4a9e6d00b97fee26611d
Hiran Theo <hirantheodozio@gmail.com>
2024-09-24 18:02:02 -0300

    inPalindrome(Str) update function

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+    const checkPalindrome = (str) => {
+        let result = '';
+        for (let i = 0; i<str.length / 2; i++ ) {
+            if (str[i] !== str[str.length -i -1]){
+                return result = false
+            } 
+            result = true
+        }
+        return result
+    }
+
 }
+console.log(isPalindrome("arara"))
+    // Arara
+    // Ovo
+    // Anilina
+    // Frases:
+
+    // A sacada da casa
+    // Ame o poema
+    // A mala nada na lama
+    // A torre da derrota
 
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+   let array = []
+
 }
